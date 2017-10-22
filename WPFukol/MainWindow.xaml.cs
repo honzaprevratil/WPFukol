be2e7c3a3983a1c9dc853ea0bce2d30e5deff4a8
honzikprevratil <honzikprevratil@gmail.com>
2017-10-22 17:36:51 +0200

    master 1

--- WPFukol/MainWindow.xaml.cs
+++ WPFukol/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WPFukol
 {
@@ -20,9 +21,112 @@ namespace WPFukol
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int RightButton = 2;
+        public int Level = 0;
+        public int PointsPerClick = 50;
+        public int TimeProm = 60;
+        int[] LevelsNumber = { 10, 20, 50 };
+
         public MainWindow()
         {
             InitializeComponent();
+            AppendQuestion();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0,0,1);
+            timer.Tick += (sender, args) => { TickDown(TimeProm); };
+            timer.Start();
+        }
+        private void TickDown(int c)
+        {
+            TimeProm = TimeProm - 10;
+            Timebar.Value = TimeProm;
+            if (TimeProm < 0)
+            {
+                GameOver("YOU LOST!!");
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Click(1);
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Click(2);
+        }
+        private void Close_Game(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Start_New_Game(object sender, RoutedEventArgs e)
+        {
+            Level = 0;
+            TimeProm = 60;
+            Progress.Value = 0;
+            AppendQuestion();
+        }
+        private void GameOver(string title)
+        {
+            question.Content = title;
+            But1.Content = "NEW GAME";
+            But1.Click -= Button_Click_1;
+            But1.Click += Start_New_Game;
+            But2.Content = "QUIT";
+            But2.Click -= Button_Click_2;
+            But2.Click += Close_Game;
+        }
+        private void Click(int clickedBut)
+        {
+            if (clickedBut == RightButton)
+            {
+                if (Progress.Value < (100 - PointsPerClick))
+                {
+                    Progress.Value += PointsPerClick;
+                } else
+                {
+                    Progress.Value = 0;
+                    if (Level < (LevelsNumber.Length - 1))
+                    {
+                        Level++;
+                    } else
+                    {
+                        question.Content = "YOU WON!!";
+                    }
+                }
+            } else
+            {
+                if (Progress.Value >= PointsPerClick)
+                {
+                    Progress.Value -= PointsPerClick;
+                }
+            }
+            if ((string)question.Content == "YOU WON!!")
+            {
+                GameOver("YOU WON!!");
+            } else
+            {
+                AppendQuestion();
+            }
+        }
+        private void AppendQuestion()
+        {
+            Random rand = new Random();
+            MathLogic Math = new MathLogic(); 
+            int[] numbers = Math.NumbersByLevel(Level, LevelsNumber);
+            question.Content = "How many is " + numbers[0] + " + " + numbers[1];
+            LevelDisplay.Content = "("+(Level+1)+"/"+(LevelsNumber.Length)+")";
+            if (rand.Next(0,2) == 1)
+            {
+                RightButton = 2;
+                But2.Content = numbers[0] + numbers[1];
+                But1.Content = rand.Next(0, 2) == 1 ? numbers[0] + numbers[1] + rand.Next(1,10) : numbers[0] + numbers[1] - rand.Next(1, 10);
+            }
+            else
+            {
+                RightButton = 1;
+                But1.Content = numbers[0] + numbers[1];
+                But2.Content = rand.Next(0, 2) == 1 ? numbers[0] + numbers[1] + rand.Next(1,10) : numbers[0] + numbers[1] - rand.Next(1, 10);
+            }
         }
     }
 }
---
+++ WPFukol/MathLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFukol
+{
+    class MathLogic
+    {
+        public int[] NumbersByLevel(int level, int[] levelsNumber)
+        {
+            Random rand = new Random();
+            int number1 = int.Parse(""+rand.Next(1, levelsNumber[level]));
+            int number2 = int.Parse(""+rand.Next(1, levelsNumber[level]));
+            int[] numbers = {number1, number2 };
+            return numbers;
+
+        }
+    }
+}
