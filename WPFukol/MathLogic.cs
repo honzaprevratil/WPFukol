88574928ec9957c4f0f6a9e1ccdd75ab09af93e6
honzikprevratil <honzikprevratil@gmail.com>
2017-11-05 11:22:40 +0100

    OK

--- WPFukol/MathLogic.cs
+++ WPFukol/MathLogic.cs
@@ -10,7 +10,7 @@ namespace WPFukol
     {
         public int[] NumbersByLevel(int level, int[] levelsNumber)
         {
-            Random rand =new Random();
+            Random rand = new Random();
             int number1 = int.Parse(""+rand.Next(1, levelsNumber[level]));
             int number2 = int.Parse(""+rand.Next(1, levelsNumber[level]));
             int[] numbers = {number1, number2 };
